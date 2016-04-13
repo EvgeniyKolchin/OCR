af228e18361a952970bf9ff48823c06a10306f5e
Evgeniy Kolchin <zhenya1994@list.ru>
2016-04-13 12:54:51 +0300

    add new login page

--- run.py
+++ run.py
@@ -1,13 +1,27 @@
-from flask import Flask, url_for,  render_template
+from flask import Flask, url_for,  render_template, flash, redirect
+from forms import LoginForm
+
 app = Flask(__name__)
+app.config.from_object('config')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+#@app.route("/")
+#def hello():
+    #return "Hello World!"
 
-@app.route("/ad")
+@app.route("/index")
 def show_entries():
-    return render_template('layout.html')
+    return render_template('index.html')
+    
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
 
 if __name__ == "__main__":
     app.run(debug=True)
